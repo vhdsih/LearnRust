b6c1dc2e4ef1bc869e04612821e0869f069a2f87
vhdsih <vhdsih@hotmail.com>
2021-06-16 01:04:01 +0800

    init

---
+++ code/hello_world/main.rs
@@ -0,0 +1,10 @@
+/*
+ * @Author: zhangdongchang
+ * @Date: 2021-06-16 00:57:15
+ * @LastEditTime: 2021-06-16 00:58:42
+ * @FilePath: \code\hello_world\main.rs
+ */
+
+ fn main() {
+    println!("hello, world!");    
+ }
---
+++ rust.md
@@ -0,0 +1,12 @@
+# Learning Rust
+
+在编译阶段捕捉潜在的BUG，
+
+工具：
+
+cargo 依赖管理和构建工具
+
+rustfmt 代码风格
+
+Rust Language server
+
